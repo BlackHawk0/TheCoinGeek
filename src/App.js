df3cb836a037375ec6925e7a643546c871ccebe2
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-10-07 13:42:38 +0300

    outline restructuring

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-
+import React, {useState, useEffect} from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
-import Feeder from './container/Feeder';
+import Coins from './components/Coins'
 
 function App() {
-  return (
-    <>
-      <NavBar />
-      <Feeder />
-    </>
-  );
+    const [coins, setCoins] = useState([])
+    const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+    useEffect(()=>{
+        fetch(url)
+        .then(res=> res.json())
+        .then(data => setCoins(data))
+        .catch((error )=> console.log('Error:', error))
+    },[])
+    console.log(coins);
+
+    return (
+        <>
+        <NavBar />
+        <Routes>
+            <Route path='' element={<Coins coins={coins}/>}/>
+        </Routes>
+            
+        </>
+    );
 }
 
 export default App;
--- src/container/Feeder.js
+++ src/container/Feeder.js
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react'
+import { Route, Routes } from 'react-router-dom'
 import Coins from '../components/Coins'
 
 function Feeder() {
     const [coins, setCoins] = useState([])
-    const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+    const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
     useEffect(()=>{
         fetch(url)
         .then(res=> res.json())
@@ -13,10 +14,13 @@ function Feeder() {
     console.log(coins);
 
     return (
-        <div>
-            <Coins coins={coins}/>
-        </div>
+        <>
+        <Routes>
+            <Route path='' element={<Coins coins={coins}/>}/>
+        </Routes>
+            
+        </>
     )
 }
 
-export default Feeder
+// export default Feeder
