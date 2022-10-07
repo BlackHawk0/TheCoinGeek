39298f508b6796df18cf2a59bc9a3d5847838dae
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-10-07 16:52:15 +0300

    style the coin component

--- src/routing/Coin.js
+++ src/routing/Coin.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import {useParams} from 'react-router-dom'
+import '../index.css'
 
 const Coin = ()=> {
 
@@ -15,7 +16,19 @@ const Coin = ()=> {
         .catch(error => console.log(error))
     },[])
     return (
-        <div>{coin.id}</div>
+        <div>
+            <div className='flex flex-col bg-gray-900 rounded-lg shadow pt-2.5 px-2.5 max-w-3xl'>
+                <div className='flex flex-col bg-gray-900 rounded-lg shadow pt-2.5 px-2.5 max-w-3xl'>
+                    <h1>{coin.name}</h1>
+                </div>
+                <div>
+                    <div>
+                        <span className='rank'>Rank #</span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
