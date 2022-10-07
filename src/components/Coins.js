6c79481a7dd20fe52b2b9dbd2aeaedbe4cf433e8
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-10-07 11:48:40 +0300

    fetch coin data

---
+++ src/components/Coins.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Coins() {
+    return (
+        <div>
+            <div>
+                <div>
+                    <p>Coin</p>
+                    <p>Price</p>
+                    <p>24h</p>
+                    <p>Volume</p>
+                    <p>Market Cap</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Coins
---
+++ src/container/Feeder.js
@@ -0,0 +1,21 @@
+import React, {useEffect, useState} from 'react'
+import Coins from '../components/Coins'
+
+function Feeder() {
+    const [coins, setCoins] = useState([])
+    const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+    useEffect(()=>{
+        fetch(url)
+        .then(res=> res.json())
+        .then(data => setCoins(data))
+        .catch((error )=> console.log('Error:', error))
+    },[])
+
+    return (
+        <div>
+            <Coins coins={coins}/>
+        </div>
+    )
+}
+
+export default Feeder
